15f7f47d8de66a7c5492a7cc221946bcfaa1cc4f
Maxwell Seefeld <77420027+maxseefeld@users.noreply.github.com>
2021-01-27 18:56:28 -0500

    Add files via upload
    basic chroot script for gentoo

---
+++ Linux/Gentoo Linux/chroot.sh
@@ -0,0 +1,13 @@
+﻿#!bin/bash
+mkdir /mnt/mychroot 
+mount /dev/nvme0n1p3 /mnt/mychroot
+mount --rbind /dev /mnt/mychroot/dev 
+mount --make-rslave /mnt/mychroot/dev 
+mount -t proc /proc /mnt/mychroot/proc
+mount --rbind /sys /mnt/mychroot/sys
+mount --make-rslave /mnt/mychroot/sys
+mount --rbind /tmp /mnt/mychroot/tmp 
+cp /etc/resolv.conf /mnt/mychroot/etc
+chroot /mnt/mychroot /bin/bash
+env-update && . /etc/profile
+export PS1="(chroot) $PS1"
