779307173184f393f12064e02e57d6f3d91b490f
Maxwell Seefeld <77420027+maxseefeld@users.noreply.github.com>
2021-01-27 18:59:50 -0500

    Add files via upload

---
+++ Linux/Gentoo Linux/gentooo update.c
@@ -0,0 +1,17 @@
+//maxwell seefeld 
+//1/27/2021
+//gentoo update script
+
+#include<stdlib.h>
+int main()
+{
+
+	system("emerge --sync");
+    system("emerge -avuDN --with-bdeps y --keep-going world");
+    system("etc-update");
+    system("lafilefixer --justfixit | grep -v skipping ");
+    system("emerge -av --depclean");
+    system("revdep-rebuild ");
+    system("eclean -d distfiles");
+	return 0;
+}
